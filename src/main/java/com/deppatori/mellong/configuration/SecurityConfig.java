d06da2c545493f4db6ee83fc9553a64aa25e2aa0
Tommy Toban <tommy.deppatori@gmail.com>
2019-06-11 10:55:37 +0700

    add jwt

--- src/main/java/com/deppatori/mellong/configuration/SecurityConfig.java
+++ src/main/java/com/deppatori/mellong/configuration/SecurityConfig.java
@@ -9,56 +9,95 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    UserDetailsService userDetailService;
+//    @Autowired
+//    UserDetailsService userDetailService;
+
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .csrf().disable()
+//                .authorizeRequests()
+//                .antMatchers("/admin/**").hasAnyRole("ADMIN")
+////	          .antMatchers("/user_list").hasRole("USER")
+//               .antMatchers("/","/login","/test").permitAll()
+//                .antMatchers("/api/v1/users/**").permitAll()
+////                .antMatchers(HttpMethod.DELETE,"/api/v1/users/**").permitAll()
+////                .antMatchers(HttpMethod.PUT,"/api/v1/users/**").permitAll()
+////                .antMatchers(HttpMethod.DELETE,"/api/v1/users/**").permitAll()
+////                .antMatchers(HttpMethod.GET,"/api/v1/users/**").permitAll()
+//
+//                .anyRequest().authenticated()
+//                .and()
+//                .formLogin()
+//                .loginPage("/login")
+//                .permitAll()
+//                .and()
+//                .logout()
+//                //  .logoutSuccessUrl("/login")
+//                .permitAll()
+//                .and()
+//                .exceptionHandling().accessDeniedPage("/access-denied");
+//    }
+
+
+
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.userDetailsService(userDetailService).passwordEncoder(passwordEncoder());
+//    }
+
+//
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
+        http.cors().and()
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/admin/**").hasAnyRole("ADMIN")
-//	          .antMatchers("/user_list").hasRole("USER")
-               .antMatchers("/","/login","/test").permitAll()
-                .antMatchers("/api/v1/users/**").permitAll()
-//                .antMatchers(HttpMethod.DELETE,"/api/v1/users/**").permitAll()
-//                .antMatchers(HttpMethod.PUT,"/api/v1/users/**").permitAll()
-//                .antMatchers(HttpMethod.DELETE,"/api/v1/users/**").permitAll()
-//                .antMatchers(HttpMethod.GET,"/api/v1/users/**").permitAll()
-
+                .antMatchers("/api/public").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .and()
-                .logout()
-                //  .logoutSuccessUrl("/login")
-                .permitAll()
-                .and()
-                .exceptionHandling().accessDeniedPage("/access-denied");
+                .addFilter(new JwtAuthenticationFilter(authenticationManager()))
+                .addFilter(new JwtAuthorizationFilter(authenticationManager()))
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
-
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailService).passwordEncoder(passwordEncoder());
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password(passwordEncoder().encode("password"))
+                .authorities("ROLE_USER");
     }
-
-
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+
+        return source;
     }
 }
---
+++ src/main/java/com/deppatori/mellong/controller/PublicController.java
@@ -0,0 +1,17 @@
+package com.deppatori.mellong.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/public")
+public class PublicController {
+
+    @GetMapping
+    public String getMessage(){
+        return "Hello from public ...";
+    }
+
+
+}
